7e7bc913f5d40c0e70c6ed5c82b7489f6c72aa10
Kirill Mukhin <m.kirya05@gmail.com>
2023-11-19 22:13:32 -0500

    Firstly just writing Account class

--- user_interaction.py
+++ user_interaction.py
@@ -1,36 +1,34 @@
-class Bank():
-    def showAccountMenu():
-        while True:
-            print("Welcome to Your Account, how I can help you today?")
-            print("Check Balance")
-            print("Deposit")
-            print("Withdraw")
-            print("Exit account")
-            print("Type in option you want as you see to choose it!")
-            responce = input()
-            if responce == "Exit Account":
-                print("Thank you for using Super Bank!")
-                break
-            elif responce == "Check Balance":
-                print("Your balance it:")
-            elif responce == "Deposit":
-                print("How much you want to Deposit?")
-                deposit = int(input("Enter the amount:"))
-            elif responce == "Withdraw":
-                print("How much you want to Withdraw?")
-                withdraw = int(input("Enter the amount:"))
+def showAccountMenu():
+    while True:
+        print("Welcome to Your Account, how I can help you today?")
+        print("Check Balance")
+        print("Deposit")
+        print("Withdraw")
+        print("Exit account")
+        print("Type in option you want as you see to choose it!")
+        responce = input()
+        if responce == "Exit Account":
+            print("Thank you for using Super Bank!")
+            break
+        elif responce == "Check Balance":
+            print("Your balance it:")
+        elif responce == "Deposit":
+            print("How much you want to Deposit?")
+            deposit = int(input("Enter the amount:"))
+        elif responce == "Withdraw":
+            print("How much you want to Withdraw?")
+            withdraw = int(input("Enter the amount:"))
         
-
-    def showMainMenu():
-        while True:
-            print("Welcome to Super Bank!\n Choose your options:")
-            print("Select account")
-            print("Exit")
-            print("Type in option you want as you see to choose it!")
-            responce = input()
-            if responce == "Select account":
-                print("Please enter your account number:")
-                showAccountMenu()
-            elif responce == "Exit":
-                print("Thank you for using Super Bank!")
-                break
+def showMainMenu():
+    while True:
+        print("Welcome to Super Bank!\n Choose your options:")
+        print("Select account")
+        print("Exit")
+        print("Type in option you want as you see to choose it!")
+        responce = input()
+        if responce == "Select account":
+            print("Please enter your account number:")
+            showAccountMenu()
+        elif responce == "Exit":
+            print("Thank you for using Super Bank!")
+            break
