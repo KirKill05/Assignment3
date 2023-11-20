1a773559a9326f1aa16193167838175e47214e28
Kirill Mukhin <m.kirya05@gmail.com>
2023-11-19 23:23:42 -0500

    Added getters and setters + fised variable names

--- Account.py
+++ Account.py
@@ -1,30 +1,47 @@
 class Account:
-    def __init__(self, account_holder, balance=0):
-        self.account_holder = account_holder
-        self.balance = balance
+    def __init__(self, _accountNumber, _accountHolderName, _rateOfInterest, _currentBalance):
+        self._accountNumber = _accountNumber
+        self._accountHolderName = _accountHolderName
+        self._rateOfInterest = _rateOfInterest
+        self._currentBalance = _currentBalance
+
+    def getAccountNumber(self):
+        return self._accountNumber
+    
+    def getAccountHolderName(self):
+        return self._accountHolderName
+    
+    def getRateOfInterest(self):
+        return self._rateOfInterest
+    
+    def getCurrentBalance(self):
+        return self._currentBalance
+    
+    def setAccountHolderName(self, newAccountHolderName):
+        self._accountHolderName = newAccountHolderName
+
+    def setRateOfInterest(self, newRateOfInterest):
+        self._accountHolderName = newRateOfInterest
 
     def deposit(self, amount):
-        self.balance += amount
+        self._currentBalance += amount
 
     def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
+        if amount <= self._currentBalance:
+            self._currentBalance -= amount
             return True
         else:
             print("Insufficient funds")
             return False
-
-    def get_balance(self):
-        return self.balance
     
 class SavingsAccount(Account):
-    def __init__(self, account_holder, balance=0, min_balance=0):
-        super().__init__(account_holder, balance)
-        self.min_balance = min_balance
+    def __init__(self, _accountHolderName, _currentBalance, _minimumBalance):
+        super().__init__(_accountHolderName, _currentBalance)
+        self._minimumBalance = _minimumBalance
 
     def withdraw(self, amount):
-        if amount <= self.balance - self.min_balance:
-            self.balance -= amount
+        if amount <= self._currentBalance - self._minimumBalance:
+            self._currentBalance -= amount
             return True
         else:
             print("Transaction rejected. Below minimum balance.")
@@ -32,13 +49,13 @@ class SavingsAccount(Account):
 
 
 class ChequingAccount(Account):
-    def __init__(self, account_holder, balance=0, overdraft_limit=0):
-        super().__init__(account_holder, balance)
-        self.overdraft_limit = overdraft_limit
+    def __init__(self, _accountHolderName, _currentBalance, _overdraftLimit):
+        super().__init__(_accountHolderName, _currentBalance)
+        self._overdraftLimit = _overdraftLimit
 
     def withdraw(self, amount):
-        if amount <= self.balance + self.overdraft_limit:
-            self.balance -= amount
+        if amount <= self._currentBalance + self._overdraftLimit:
+            self._currentBalance -= amount
             return True
         else:
             print("Transaction rejected. Overdraft limit exceeded.")
